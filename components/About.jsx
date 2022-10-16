20ef724978fbde2b2c2ffbde2cc241085ba26cb8
eldhoittangeorge <eldhoittangeorge@gmail.com>
2022-10-16 13:43:49 +0530

    Added text content

--- components/About.jsx
+++ components/About.jsx
@@ -11,25 +11,43 @@ const About = () => {
           <p>
             I am passionate about Machine Learning especially, NLP. My
             background is in Artificial Intelligence and Data Science. Currently
-            I am working as an AI research inter at collabll. I worked as a
-            Machine learing Intern at TCS for 6 months. At TCS I working on
-            building chatbots for websites. Before starting my career in AI I
-            worked as a software engineer at a e-comerce company called Evelor
-            Corporations.
+            I am working as an AI Research Intern at collabll. I am working on
+            creating a question answering system for Indian law. This enables
+            user to search for any particular law related to their issues. The
+            question answering system works with most of the indic languages.
           </p>
           <ul className="list-disc list-inside mt-2">
             <li className="mt-1">
-              💼 Right now, I am working on building a question answering system
-              for Indian law.
+              🧑‍🎓 I am a graduate with a Master's in Artificiall Intelligence and
+              Machine Learning
+            </li>
+            <li className="mt-1">
+              🏠 Keral native, with a passionate for Artificiall Intelligence
+              especially in NLP
+            </li>
+            <li className="mt-1">
+              👨‍💻 Previously I worked as a Machine Learning Intern at TCS for 6
+              months. At TCS I worked on bulilding an NLP chatbot for website.{" "}
+            </li>
+            <li className="mt-1">
+              👨‍💻 I also worked as a Data Analyst/Software Engineer at Envelor
+              Corporation for 1 year
+            </li>
+            <li className="mt-1">
+              🔮 As for my future, I hope to one day work as an AI researcher in
+              an AI focued company
+            </li>
+            <li className="mt-1">
+              ⛰️ In my free time, I read fantacy and fiction books
             </li>
           </ul>
         </div>
         <div className="flex flex-2 gap-4 flex-col">
-          {/* <div className={aboutStyle.cv_box} /> */}
           <Image
             src={resumeThumbnail}
             title="Resume"
             alt="Thumbnail of resume"
+            // height={300}
           />
           <a
             className="bg-slate-900 rounded-2xl p-2 text-white text-sm text-center"
--- components/TechnologyItem.jsx
+++ components/TechnologyItem.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import technologyItemStyle from "../styles/TechnologyItem.module.css";
 
 const TechnologyItem = ({ text }) => {
   return (
-    <div className={technologyItemStyle.container}>
-      <h5 className={technologyItemStyle.textStyle}>{text}</h5>
+    <div className="flex bg-slate-800 rounded-2xl text-white text-sm text-center">
+      <p className="m-2">{text}</p>
     </div>
   );
 };
